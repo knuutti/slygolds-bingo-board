aa3c71a806cf537948ba0142ebe875d254c76f28
Tobias Gies <tobias@tobiasgies.de>
2022-04-20 19:59:26 +0200

    Add generic list of bingo items

---
+++ bingo-items.js
@@ -0,0 +1,7 @@
+let bingoItems = [
+    // Add bingo items to this list. If you must use the double quote character (") in an item, escape it like this: \"
+    "Biggoron Sword found",
+    "Hookshot from Roofio",
+    "Second magic in Shadow Temple"
+    // For compatibility reasons, the very last item in the list should not have a comma after it.
+]
