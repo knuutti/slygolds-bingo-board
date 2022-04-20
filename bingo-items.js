3a73f3657ffb50d3c53c3899d775c94cd4a5a6c5
dastar512 <39785314+dastar512@users.noreply.github.com>
2022-04-20 14:12:09 -0400

    Update bingo-items.js

--- bingo-items.js
+++ bingo-items.js
@@ -2,8 +2,8 @@ let bingoItems = [
     // Add bingo items to this list. If you must use the double quote character (") in an item, escape it like this: \"
     "Biggoron Sword found",
     "Hookshot from Roofio",
-    "Second magic in Shadow Temple"
-    "Item or Song is VANILLA"
-    "30+ skulls logically required"
+    "Second magic in Shadow Temple",
+    "Item or Song is VANILLA",
+    "30+ skulls logically required",
     // For compatibility reasons, the very last item in the list should not have a comma after it.
 ];
