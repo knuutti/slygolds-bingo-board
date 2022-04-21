42b061fd3ed1415c5c77458684f4e0706a5c15bd
dastar512 <39785314+dastar512@users.noreply.github.com>
2022-04-20 21:53:33 -0400

    Update bingo-items.js

--- bingo-items.js
+++ bingo-items.js
@@ -4,6 +4,23 @@ let bingoItems = [
     "Hookshot from Roofio",
     "Second magic in Shadow Temple",
     "Item or Song is VANILLA",
-    "30+ skulls logically required"
+    "30+ skulls",
+    "Ice Cavern entered before 90 minutes",
+    "Chickens (Kakariko)",
+    "GTG Toilet",
+    "Comms off the rails", //too subjective?
+    "Emotional Support Fish",
+    "Comms makes a pun", //puts intention in charge of bingo rather than luck
+    "Richard's mom is a cheapskate (20 rupees or less)",
+    "OoT is picked up",
+    "Morpha Quick Grab",
+    "Tennis Troll (PG or GD)",
+    "Comms mentions MQ dungeons",
+    "Deku Shield burns away",
+    "Link gets Frozen",
+    "Forced to wait for Time of Day",
+    "[insert dungeon] Key Logic"
+    "[Single check location] is foolish"
+    "Void out",  
     // For compatibility reasons, the very last item in the list should not have a comma after it.
 ];
