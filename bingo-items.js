3ccf9209bc584789a3fea24944b0421d937e74a2
dastar512 <39785314+dastar512@users.noreply.github.com>
2022-04-20 22:19:52 -0400

    Update bingo-items.js

--- bingo-items.js
+++ bingo-items.js
@@ -19,8 +19,28 @@ let bingoItems = [
     "Deku Shield burns away",
     "Link gets Frozen",
     "Forced to wait for Time of Day",
-    "[insert dungeon] Key Logic"
-    "[Single check location] is foolish"
+    "[insert dungeon] Key Logic" //probably just delete this?
+    "[Single check location] is foolish" //dunno how to insert locations like the note sheet does?
     "Void out",  
+    "HBA Score < 1550",
+    "Songs R Hard",
+    "Meme% located",
+    "Double Defense located",
+    "Tunic/Wallet in Path location", 
+    "Stone Dungeon Boss required by Logic",
+    "Stream intro full of bangers (Free Space)", //can the Free Space be overwritten with this one?
+    "Chicken Abandons Link",
+    "Iron Knuckle Death",
+    "Burn timer death",
+    "Drown Timer Death", 
+    "Missing Always Hint at 90 minutes",
+    "Missing Path Hint at 90 minutes", 
+    "Hard AD discovered after 60 minutes", 
+    "Racer gets bailed out after missing logic",
+    "Dark Link found a way",
+    "Foolish Hint in the Foolish location", 
+    "Path hints leading to same item",
+    "Oops, AD!",
+    "Oops, World Tour!"
     // For compatibility reasons, the very last item in the list should not have a comma after it.
 ];
