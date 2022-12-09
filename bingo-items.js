732eb3077eb6cbc7fd1e6dd1fd73f29823ebb3ee
Knuutti <96994953+Knuutti@users.noreply.github.com>
2022-12-09 21:00:07 +0200

    Updated bingo items

--- bingo-items.js
+++ bingo-items.js
@@ -16,6 +16,21 @@ var bingoItems = [
     "Chat is lewd",
     "carmWarm",
     "Oggy is fat",
+    "Someone voids",
+    "Console crash",
+    "carmWarm",
+    "Remo does Slyence",
+    "chat mentions sly 5",
+    "Galleth says Fortnite",
+    "Runner misses any form of Paris Skip - Sly 1",
+    "missedsquareboost.com",
+    "Runner fails Pirate Talk",
+    "SlyGolds.com sponsorship/shoutout",
+    "Scam Train",
+    "Gift sub(s)",
+    "Chat is lewd",
+    "carmWarm",
+    "Oggy is fat",
     "TRUCKIN"
 
     // For compatibility reasons, the very last item in the list should not have a comma after it.
